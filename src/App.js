e41697132bf4773f0ae260404075c70c40bd9ac5
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-12-09 15:10:17 +1100

    completed fetch request to HTTP request using the fetch API to retrieve weather data

--- src/App.js
+++ src/App.js
@@ -7,16 +7,27 @@ function App() {
 
 const [lat, setLat] = useState([]);
 const [long, setLong] = useState([]);
+const [data, setData] = useState([])
 
-useEffects(() => {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    setLat(position.coords.latitude);
-    setLong(position.coords.longitude);
-  });
+useEffect(() => {
+  const fetchData = async () => {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLong(position.coords.longitude);
+    });
 
-  console.log("Latitude is: ", lat);
-  console.log("Longitude is: ", long);
-}, [lat, long]);
+    await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+    .then(res => res.json())
+    .then(result => {
+      setData(result)
+      console.log(result);
+    });
+  }
+  fetchData();
+}, [lat,long])
+  
+
+
 
 
 
